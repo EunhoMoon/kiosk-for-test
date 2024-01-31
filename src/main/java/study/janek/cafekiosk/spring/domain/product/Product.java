8cdbb423ab28121136ba43cd359f40a83d2546ba
Janek <eunho.moon@gmail.com>
2024-01-31 21:23:59 +0900

    persistence layer test

--- src/main/java/study/janek/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/study/janek/cafekiosk/spring/domain/product/Product.java
@@ -2,6 +2,7 @@ package study.janek.cafekiosk.spring.domain.product;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import study.janek.cafekiosk.spring.domain.BaseEntity;
@@ -27,4 +28,19 @@ public class Product extends BaseEntity {
 
     private int price;
 
+    @Builder
+    private Product(
+        Long id,
+        String productNumber,
+        ProductType type,
+        ProductSellingStatus sellingStatus,
+        String name,
+        int price
+    ) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
